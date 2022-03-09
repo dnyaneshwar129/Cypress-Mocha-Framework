53cccb63f69052525ef10019348abbc7245bd041
Dnyaneshwar Raut <rautdnyaneshwar@gmail.com>
2022-03-09 14:36:23 +0530

    Custome command

---
+++ cypress/integration/my-work/Customcommand.js
@@ -0,0 +1,58 @@
+/// <reference types="cypress" />
+
+
+describe('Custome Command', function () {
+
+  
+    
+        it('Custome Command login', function()
+        {
+            cy.login('admin@yourstore.com', 'admin')
+            cy.title().should('be.equal', 'Dashboard / nopCommerce administration')
+            cy.log('This is for Custome Command login')
+
+    
+        })
+
+
+           
+        it('invalid login', function()
+        {
+         
+
+            cy.login('admin@yourstore.com', 'invalid')
+            cy.title().should('not.be.equal', 'Dashboard / nopCommerce administration')
+
+    
+        })
+
+        it('Add Customer', function()
+        {
+
+            cy.login('admin@yourstore.com', 'admin')
+
+            cy.log('This is for add customer')
+    
+        })
+
+        it('Edit  Customer', function()
+        {
+
+            cy.login('admin@yourstore.com', 'admin')
+
+            cy.log('This is for Edit customer')
+    
+        })
+
+        it('Delete  Customer', function()
+        {
+
+            cy.login('admin@yourstore.com', 'admin')
+
+            cy.log('This is for Delete customer')
+    
+        })
+
+
+
+})
