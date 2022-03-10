5baac983727088376bfb153d7e348ded0b34258e
Dnyaneshwar Raut <rautdnyaneshwar@gmail.com>
2022-03-10 10:44:43 +0530

    Page Object concept

---
+++ cypress/integration/PageObjects/LoginPage.js
@@ -0,0 +1,36 @@
+class LoginPage
+{
+    visit()
+    {
+        cy.visit('https://admin-demo.nopcommerce.com/login')
+
+
+    }
+
+
+    EnterEmailaddress(emailvalue)
+    {
+        const emailfield = cy.get('input[name="Email"]')
+        emailfield.clear().type(emailvalue) 
+        return this
+    }
+
+    EnterPassword(paswordlvalue)
+    {
+        const emailfield = cy.get('input[name="password"]')
+        emailfield.clear().type(paswordlvalue) 
+        return this
+    }
+
+
+    ClickSubmit()
+    {
+       const submitbtn = cy.get('.login-button')
+       submitbtn.click()
+        
+    }
+
+   
+
+}
+export default LoginPage  // for accessing this calss in test cases
